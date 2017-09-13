50a0db20d173d6a0c77a93c49a89b7a25906949a
jabelar <julian_abelar@hotmail.com>
2017-09-13 00:03:34 -0700

    updated todo list to correct info on item models

--- src/main/java/com/blogspot/jabelarminecraft/foodrevamp/items/todo.java
+++ src/main/java/com/blogspot/jabelarminecraft/foodrevamp/items/todo.java
@@ -5,7 +5,7 @@ public class todo {
  * copy item class, rename file and set name.
  * in item registry create instances all caps
  * in item registry go to the initialize method and add to the array
- * in client proxy item renderers add item
+ * in item registry register the item models
  * go in assets and add name to lang file
  * go into the models and create file with proper name that points to texture
  * 
